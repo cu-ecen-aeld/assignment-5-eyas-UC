b6b1e60a71328c44969b4c443dd80935b427c7f6
eyas-UC <eyas077@gmail.com>
2025-11-01 09:47:39 +0000

    adding new aesdsocket with 0.0.0.0

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'b2fcd9eeb1cbaafa579fcd8a65bbc0fbbde7f383' # assignment-5-socket in assignments-3 repo
+AESD_ASSIGNMENTS_VERSION = 'af66242985f68cc1aec7318cc0975f4b852504fd' # assignment-5-socket in assignments-3 repo
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
