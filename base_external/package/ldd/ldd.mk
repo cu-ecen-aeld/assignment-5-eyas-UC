da2466edb9f840da89241c72e68a0ce0ecc673a0
Eyas Elhashmi <eyas077@gmail.com>
2026-05-07 20:35:10 +0100

    clean ldd.mk

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -3,18 +3,14 @@ LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-eyas-UC.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = NO
 
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
-endef
+
 LDD_MODULES_SUBDIRS = misc-modules scull
 LDD_MODULE_SUBDIRS  = $(LDD_MODULES_SUBDIRS)
 
 
-# define LLD_INSTALL_TARGET_CMDS
-#  	$(INSTALL) -m 0755 $(@D)/init $(TARGET_DIR)/etc/modules/$(LINUX_VERSION_PROBED)/
-# 	$(INSTALL) -m 0755 $(@D)/init $(TARGET_DIR)/etc/modules/$(LINUX_VERSION_PROBED)/
-# 	$(INSTALL) -m 0755 $(@D)/server/S99aesdsocket $(TARGET_DIR)/etc/init.d/S99aesdsocket
-# endef
+define LLD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/S98lldmodules $(TARGET_DIR)/etc/init.d/S98lldmodules 
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
